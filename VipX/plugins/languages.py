82d031e76d702becf8aa947c0befccba0c919984
DAXXSIR <137784719+DAXXSIR@users.noreply.github.com>
2023-07-28 20:53:41 +0530

    𝐃𝐀𝐗𝐗

--- VipX/plugins/languages.py
+++ VipX/plugins/languages.py
@@ -16,7 +16,7 @@ def lanuages_keyboard(_):
     keyboard = InlineKeyboard(row_width=2)
     keyboard.row(
         InlineKeyboardButton(
-            text="🇦🇺 ᴇɴɢʟɪsʜ 🇦🇺",
+            text="ᴇɴɢʟɪsʜ 🇦🇺",
             callback_data=f"languages:en",
         ),
     )
