a657b5ec7251ae74c9074b58bd33d338f070de62
DAXXSIR <137784719+DAXXSIR@users.noreply.github.com>
2023-08-01 17:01:35 +0530

    𝐃ɛʂꙷʈͦɪͧղͬ𝐘...

--- VipX/utils/thumbnails.py
+++ VipX/utils/thumbnails.py
@@ -11,7 +11,10 @@ from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from VipX import app
-YOUTUBE_IMG_URL = ["https://graph.org/file/703e54ed41a78e9ae3d79.jpg" ]
+YOUTUBE_IMG_URL = [
+    "https://graph.org/file/703e54ed41a78e9ae3d79.jpg"
+
+]
 
 
 def changeImageSize(maxWidth, maxHeight, image):
