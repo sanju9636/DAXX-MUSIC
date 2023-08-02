8b56cda53e8abdf907e4dda76dc893c55e7fa6c3
DAXXSIR <137784719+DAXXSIR@users.noreply.github.com>
2023-08-02 12:35:55 +0530

    𝐃𝐀𝐗𝐗

--- VipX/utils/inline/next.py
+++ VipX/utils/inline/next.py
@@ -1,17 +1,1 @@
-from typing import Union
-import re
-import os
-from os import getenv
-from dotenv import load_dotenv
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-import config
-from config import GROUP_USERNAME, CHANNEL_USERNAME
 
-
-def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="🕹 𝐀𝐃𝐃 𝐌𝐄 𝐁𝐀𝐁𝐘2 🕹",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
