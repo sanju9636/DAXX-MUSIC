7702f4a3d15fde2bf0bfe1750f65b18482ab7fb6
sanju9636 <141401968+sanju9636@users.noreply.github.com>
2023-08-08 08:27:52 +0530

    Update userbot.py

--- VipX/core/userbot.py
+++ VipX/core/userbot.py
@@ -48,9 +48,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("CYBERDAXXX")
-                await self.one.join_chat("CYBERDAXX")
-                await self.one.join_chat("CYBERDAXXX")
+                await self.one.join_chat("music_bot098")
+                await self.one.join_chat("music_bot098")
+                await self.one.join_chat("music_bot098")
             except:
                 pass
             assistants.append(1)
@@ -80,9 +80,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("CYBERDAXXX")
-                await self.two.join_chat("CYBERDAXX")
-                await self.two.join_chat("CYBERDAXXX")
+                await self.two.join_chat(music_bot098")
+                await self.two.join_chat("music_bot098")
+                await self.two.join_chat("music_bot098")
             except:
                 pass
             assistants.append(2)
@@ -112,9 +112,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("CYBERDAXXX")
-                await self.three.join_chat("CYBERDAXX")
-                await self.three.join_chat("CYBERDAXXX")
+                await self.three.join_chat("music_bot098")
+                await self.three.join_chat("music_bot098")
+                await self.three.join_chat("music_bot098")
             except:
                 pass
             assistants.append(3)
@@ -144,9 +144,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("CYBERDAXXX")
-                await self.four.join_chat("CYBERDAXX")
-                await self.four.join_chat("CYBERDAXXX")
+                await self.four.join_chat("music_bot098")
+                await self.four.join_chat("music_bot098")
+                await self.four.join_chat("music_bot098")
             except:
                 pass
             assistants.append(4)
@@ -176,9 +176,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("CYBERDAXXX")
-                await self.five.join_chat("CYBERDAXX")
-                await self.five.join_chat("CYBERDAXXX")
+                await self.five.join_chat("music_bot098")
+                await self.five.join_chat("music_bot098")
+                await self.five.join_chat("music_bot098")
             except:
                 pass
             assistants.append(5)
